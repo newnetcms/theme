7468fa34d62fb8872df87c02fe5f2ebb101c5e9e
KimTrien <kimtrien@gmail.com>
2024-11-10 15:16:33 +0700

    update default theme

---
+++ stubs/source-copy/public/assets/js/script.js
@@ -0,0 +1,22 @@
+function docReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+function serialize (data) {
+    let obj = {};
+    for (let [key, value] of data) {
+        if (obj[key] !== undefined) {
+            if (!Array.isArray(obj[key])) {
+                obj[key] = [obj[key]];
+            }
+            obj[key].push(value);
+        } else {
+            obj[key] = value;
+        }
+    }
+    return obj;
+}
