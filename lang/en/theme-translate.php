af3d41f42512a23ccc33b02b2bffd5dbca1cb4d2
Newnet DMB <kimtrien@gmail.com>
2026-07-24 16:13:52 +0700

    feat: Add English language translation for content management

---
+++ lang/en/theme-translate.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'model_name' => 'Content Translation',
+
+    'index' => [
+        'page_title'    => 'Content Translation',
+        'page_subtitle' => 'Content Translation',
+        'breadcrumb'    => 'Content Translation',
+    ],
+
+    'notification' => [
+        'updated' => 'Content has been saved successfully',
+    ],
+];
